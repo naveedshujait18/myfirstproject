3e108141c56b5a131304e0989c9c8c787bd19d62
Dilsh <Dilsh@10.0.0.138>
2020-12-14 20:21:36 -0600

    First Commit

---
+++ java-prac/src/com/prac/BiNumberRunner.java
@@ -0,0 +1,16 @@
+package com.prac;
+
+public class BiNumberRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BiNumber numbers = new Binumber(2, 3);
+		System.out.println(numbers.add());
+		System.out.println(numbers.multiply());
+		numbers.double();//Double both numbers
+		System.out.println(numbers.getNumber1());
+		System.out.println(numbers.getNumber2());
+
+	}
+
+}
---
+++ java-prac/src/com/prac/Binumber.java
@@ -0,0 +1,12 @@
+package com.prac;
+
+public class Binumber {
+	private int number1;
+	private int number 2;
+	
+	public BiNumber(int number1, int number2) {
+		this.number1 = number1;
+		this.number2 = number2;
+	}
+
+}
